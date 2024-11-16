49aa0824998fb6227fb71566b45c1d1fdb9ebe30
Sveta Konstantinova <svetikons@mail.ru>
2024-11-16 19:26:21 +0300

    changed blocks projects, services, added questions block

---
+++ script/accordion.js
@@ -0,0 +1,12 @@
+$(function () {
+    $('.questions__expand').on('click', function () {
+        $(this).next().slideToggle(200);
+        $expand = $(this).find('>:nth-child(2)');
+
+        if ($expand.text() == '+') {
+            $expand.text('-');
+        } else {
+            $expand.text('+');
+        }
+    });
+});
